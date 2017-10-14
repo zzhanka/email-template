1d6dde826b9067142711706f58514d0504f1fc3d
Zhanna <danunafik@gmail.com>
2017-10-14 23:27:47 +0300

    Add advantage block

--- new_year.php
+++ new_year.php
@@ -6,6 +6,10 @@ if ($isTest) {
   $img2src = 'images/new_year/maliy_budzhet.jpg';
   $img3src = 'images/new_year/sredniy_budzhet.jpg';
   $img4src = 'images/new_year/ekskluzivnie_podarki.jpg';
+  $img5src = 'images/new_year/dostavka.png';
+  $img6src = 'images/new_year/pomosh_s_maketom_obrazci.png';
+  $img7src = 'images/new_year/upakovka.png';
+  $img8src = 'images/new_year/sroki.png';
 } else {
   $img1src = '';
   $img2src = 'url2';
@@ -414,3 +418,41 @@ $xl_verticalPadding = '<div style="height: 50px; line-height: 9px; font-size: 7p
 </tr>
 </table>
 <![endif]-->
+
+
+<?= $m_verticalPadding; ?>
+<hr/>
+<?= $l_verticalPadding; ?>
+
+<?= $m_verticalPadding; ?>
+<table class="outer" align="center">
+  <tr>
+    <?= $horizontalPadding; ?>
+    <td>
+      <?php showHeaderText('<b>Кроме качественных и красивых подарков мы обещаем:</b>'); ?>
+    </td>
+    <?= $horizontalPadding; ?>
+  </tr>
+</table>
+<?= $m_verticalPadding; ?>
+
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+  <tr>
+    <td align="center" valign="top"><img src="<?=$img5src;?>" border="0" style="display: block;height:80px;max-width:100%;"/><div style="height: 10px; line-height: 10px; font-size: 7px;"></div></td>
+    <td style="width: 5px; max-width: 5px; min-width: 5px;" width="5"></td>
+    <td align="center" valign="top"><img src="<?=$img6src;?>" border="0" style="display: block;height:80px;max-width:100%;"/><div style="height: 10px; line-height: 10px; font-size: 7px;"></div></td>
+    <td style="width: 5px; max-width: 5px; min-width: 5px;" width="5"></td>
+    <td align="center" valign="top"><img src="<?=$img7src;?>" border="0" style="display: block;height:80px;max-width:100%;"/><div style="height: 10px; line-height: 10px; font-size: 7px;"></div></td>
+    <td style="width: 5px; max-width: 5px; min-width: 5px;" width="5"></td>
+    <td align="center" valign="top"><img src="<?=$img8src;?>" border="0" style="display: block;height:80px;max-width:100%;"/><div style="height: 10px; line-height: 10px; font-size: 7px;"></div></td>
+  </tr>
+  <tr>
+    <td align="center" valign="middle"><font color="#000000" face="Arial, sans-serif" style="font-size: 12px; line-height: 14px;"><span style="font-family: Arial, Tahoma, Geneva, sans-serif; color: #000000; font-size: 12px; line-height: 14px;">Оперативную доставку</span></font></td>
+    <td style="width: 5px; max-width: 5px; min-width: 5px;" width="5"></td>
+    <td align="center" valign="middle"><font color="#000000" face="Arial, sans-serif" style="font-size: 12px; line-height: 14px;"><span style="font-family: Arial, Tahoma, Geneva, sans-serif; color: #000000; font-size: 12px; line-height: 14px;">Помощь с макетом и образцы</span></font></td>
+    <td style="width: 5px; max-width: 5px; min-width: 5px;" width="5"></td>
+    <td align="center" valign="middle" align="center" valign="middle"><font color="#000000" face="Arial, sans-serif" style="font-size: 12px; line-height: 14px;"><span style="font-family: Arial, Tahoma, Geneva, sans-serif; color: #000000; font-size: 12px; line-height: 14px;">Отдельная упаковка каждого предмета</span></font></td>
+    <td style="width: 5px; max-width: 5px; min-width: 5px;" width="5"></td>
+    <td align="center" valign="middle"><font color="#000000" face="Arial, sans-serif" style="font-size: 12px; line-height: 14px;"><span style="font-family: Arial, Tahoma, Geneva, sans-serif; color: #000000; font-size: 12px; line-height: 14px;">Строгое соблюдение сроков</span></font></td>
+  </tr>
+</table>
