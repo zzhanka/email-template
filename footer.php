7e29820bf9ada2e9b04069dfd4d06a9f65a1aa3d
Zhanna <danunafik@gmail.com>
2017-10-15 15:09:50 +0300

    Footer

--- footer.php
+++ footer.php
@@ -12,56 +12,189 @@ if ($isTest) {
 ?>
 
  <!-- FOOTER (START) -->
-  
-<table border="0" cellpadding="0" cellspacing="0" style="border-bottom-color:#cccccc;border-bottom-width:1px;border-bottom-style:solid;border-top-color:#cccccc;border-top-width:1px;border-top-style:solid;border-spacing:0;font-family:sans-serif;color:#333333;" width="100%">
-  <tbody>
+
+
+
+
+<!-- TABLE (START) -->
+
+  <!--[if (gte mso 9)|(IE)]>
+  <table width="600" align="center">
+  <tr>
+  <td>
+  <![endif]-->
+  <table class="outer" align="center">
     <tr>
-      <td style="width:15px;max-width:15px;min-width:15px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" width="15"></td>
-      <td style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" valign="middle">
-      <div style="height:9px;line-height:9px;font-size:7px;"></div>
-      <a href="https://printio.ru/store/clothing/men_tshirts" style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;" target="_blank"><font color="#000000" face="Arial, sans-serif" style="font-size:14px;line-height:18px;text-decoration:underline;"><span style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;">МУЖЧИНАМ</span> </font> </a>
+      <td class="two-column">
+        <!--[if (gte mso 9)|(IE)]>
+        <table width="100%">
+        <tr>
+        <td width="50%" valign="top">
+        <![endif]-->
+        <div class="column">
+          <table width="100%">
+            <tr>
+              <td class="inner">
+                <!--[if (gte mso 9)|(IE)]>
+                <table width="600" align="center">
+                <tr>
+                <td>
+                <![endif]-->
+                <table class="outer" align="center">
+                  <tr>
+                    <td class="three-column">
+                      <!--[if (gte mso 9)|(IE)]>
+                      <table width="100%">
+                      <tr>
+                      <td width="200" valign="top">
+                      <![endif]-->
+                      
+                      <table class="column">
+                        <tr>
+                          <td class="inner">
+                            <a href="https://printio.ru/store/clothing/men_tshirts" style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;" target="_blank"><font color="#000000" face="Arial, sans-serif" style="font-size:14px;line-height:18px;text-decoration:underline;"><span style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;">МУЖЧИНАМ</span> </font> </a>
+                          </td>
+                        </tr>
+                      </table>
 
-      <div style="height:9px;line-height:9px;font-size:7px;"></div>
-      </td>
-      <td style="width:15px;max-width:15px;min-width:15px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" width="15"></td>
-      <td style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" valign="middle">
-      <div style="height:9px;line-height:9px;font-size:7px;"></div>
-      <a href="https://printio.ru/store/clothing/women_tshirts" style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;" target="_blank"><font color="#000000" face="Arial, sans-serif" style="font-size:14px;line-height:18px;text-decoration:underline;"><span style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;">ЖЕНЩИНАМ</span> </font> </a>
+                      <!--[if (gte mso 9)|(IE)]>
+                      </td><td width="200" valign="top">
+                      <![endif]-->
 
-      <div style="height:9px;line-height:9px;font-size:7px;"></div>
-      </td>
-      <td style="width:15px;max-width:15px;min-width:15px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" width="15"></td>
-      <td style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" valign="middle">
-      <div style="height:9px;line-height:9px;font-size:7px;"></div>
-      <a href="https://printio.ru/store/kids_items" style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;" target="_blank"><font color="#000000" face="Arial, sans-serif" style="font-size:14px;line-height:18px;text-decoration:underline;"><span style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;">ДЕТЯМ</span> </font> </a>
+                      <table class="column">
+                        <tr>
+                          <td class="inner">
+                            
+                            <a href="https://printio.ru/store/clothing/women_tshirts" style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;" target="_blank"><font color="#000000" face="Arial, sans-serif" style="font-size:14px;line-height:18px;text-decoration:underline;"><span style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;">ЖЕНЩИНАМ</span> </font> </a>
 
-      <div style="height:9px;line-height:9px;font-size:7px;"></div>
-      </td>
-      <td style="width:15px;max-width:15px;min-width:15px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" width="15"></td>
-      <td style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" valign="middle">
-      <div style="height:9px;line-height:9px;font-size:7px;"></div>
-      <a href="https://printio.ru/store/home_supplies" style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;" target="_blank"><font color="#000000" face="Arial, sans-serif" style="font-size:14px;line-height:18px;text-decoration:underline;"><span style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;">ДОМ И ИНТЕРЬЕР</span> </font> </a>
+                          </td>
+                        </tr>
+                      </table>
 
-      <div style="height:9px;line-height:9px;font-size:7px;"></div>
-      </td>
-      <td style="width:15px;max-width:15px;min-width:15px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" width="15"></td>
-      <td style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" valign="middle">
-      <div style="height:9px;line-height:9px;font-size:7px;"></div>
-      <a href="https://printio.ru/store/phone_cases" style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;" target="_blank"><font color="#000000" face="Arial, sans-serif" style="font-size:14px;line-height:18px;text-decoration:underline;"><span style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;">ЧЕХЛЫ</span> </font> </a>
+                      <!--[if (gte mso 9)|(IE)]>
+                      </td><td width="200" valign="top">
+                      <![endif]-->
 
-      <div style="height:9px;line-height:9px;font-size:7px;"></div>
-      </td>
-      <td style="width:15px;max-width:15px;min-width:15px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" width="15"></td>
-      <td style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" valign="middle">
-      <div style="height:9px;line-height:9px;font-size:7px;"></div>
-      <a href="https://printio.ru/store/edible" style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;" target="_blank"><font color="#000000" face="Arial, sans-serif" style="font-size:14px;line-height:18px;text-decoration:underline;"><span style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;">ПРОЧЕЕ</span> </font> </a>
+                      <table class="column">
+                        <tr>
+                          <td class="inner">
+                            <a href="https://printio.ru/store/kids_items" style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;" target="_blank"><font color="#000000" face="Arial, sans-serif" style="font-size:14px;line-height:18px;text-decoration:underline;"><span style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;">ДЕТЯМ</span> </font> </a>
+                          </td>
+                        </tr>
+                      </table>
 
-      <div style="height:9px;line-height:9px;font-size:7px;"></div>
+                      <!--[if (gte mso 9)|(IE)]>
+                      </td>
+                      </tr>
+                      </table>
+                      <![endif]-->
+                    </td>
+                  </tr>
+                </table>
+                <!--[if (gte mso 9)|(IE)]>
+                </td>
+                </tr>
+                </table>
+                <![endif]-->
+
+
+              </td>
+            </tr>
+          </table>
+        </div>
+        <!--[if (gte mso 9)|(IE)]>
+        </td><td width="50%" valign="top">
+        <![endif]-->
+        <div class="column">
+          <table width="100%">
+            <tr>
+              <td class="inner">
+
+
+                <!--[if (gte mso 9)|(IE)]>
+                <table width="600" align="center">
+                <tr>
+                <td>
+                <![endif]-->
+                <table class="outer" align="center">
+                  <tr>
+                    <td class="three-column">
+                      <!--[if (gte mso 9)|(IE)]>
+                      <table width="100%">
+                      <tr>
+                      <td width="200" valign="top">
+                      <![endif]-->
+                      
+                      <table class="column">
+                        <tr>
+                          <td class="inner">
+                            <a href="https://printio.ru/store/home_supplies" style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;" target="_blank"><font color="#000000" face="Arial, sans-serif" style="font-size:14px;line-height:18px;text-decoration:underline;"><span style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;">ДОМ И ИНТЕРЬЕР</span> </font> </a>
+                          </td>
+                        </tr>
+                      </table>
+
+                      <!--[if (gte mso 9)|(IE)]>
+                      </td><td width="200" valign="top">
+                      <![endif]-->
+
+                      <table class="column">
+                        <tr>
+                          <td class="inner">
+                            
+                            <a href="https://printio.ru/store/phone_cases" style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;" target="_blank"><font color="#000000" face="Arial, sans-serif" style="font-size:14px;line-height:18px;text-decoration:underline;"><span style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;">ЧЕХЛЫ</span> </font> </a>
+                          </td>
+                        </tr>
+                      </table>
+
+                      <!--[if (gte mso 9)|(IE)]>
+                      </td><td width="200" valign="top">
+                      <![endif]-->
+
+                      <table class="column">
+                        <tr>
+                          <td class="inner">
+                            <a href="https://printio.ru/store/edible" style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;" target="_blank"><font color="#000000" face="Arial, sans-serif" style="font-size:14px;line-height:18px;text-decoration:underline;"><span style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#000000;font-size:14px;line-height:18px;text-decoration:underline;">ПРОЧЕЕ</span> </font> </a>
+                          </td>
+                        </tr>
+                      </table>
+
+                      <!--[if (gte mso 9)|(IE)]>
+                      </td>
+                      </tr>
+                      </table>
+                      <![endif]-->
+                    </td>
+                  </tr>
+                </table>
+                <!--[if (gte mso 9)|(IE)]>
+                </td>
+                </tr>
+                </table>
+                <![endif]-->
+
+
+              </td>
+            </tr>
+          </table>
+        </div>
+        <!--[if (gte mso 9)|(IE)]>
+        </td>
+        </tr>
+        </table>
+        <![endif]-->
       </td>
-      <td style="width:15px;max-width:15px;min-width:15px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" width="15"></td>
     </tr>
-  </tbody>
-</table>
+  </table>
+  <!--[if (gte mso 9)|(IE)]>
+  </td>
+  </tr>
+  </table>
+  <![endif]-->
+
+<!-- TABLE (END) -->
+
+
+
 
 <div style="height:20px;line-height:50px;font-size:7px;"></div>
 
