816f6511aed154898cca8a8dfcd0c643b38a05b9
Zhanna <danunafik@gmail.com>
2017-10-16 16:04:52 +0300

    Footer fix

--- footer.php
+++ footer.php
@@ -63,4 +63,52 @@ if ($isTest) {
                 </tr>
               </tbody>
             </table>
+
+            <div style="height:20px;line-height:50px;font-size:7px;"></div>
+
+            <table border="0" cellpadding="0" cellspacing="0" style="border-spacing:0;font-family:sans-serif;color:#333333;" width="100%">
+              <tbody>
+                <tr>
+                  <td style="width:15px;max-width:15px;min-width:15px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" width="15"></td>
+                  <td style="padding: 0px; text-align: center;" valign="middle"><font color="#999999" face="Arial, sans-serif" style="font-size:12px;line-height:14px;"><span style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#999999;font-size:12px;line-height:14px;">Новости, конкурсы, халява и пикантные фотографии в наших соц сетях</span></font></td>
+                  <td style="width:15px;max-width:15px;min-width:15px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" width="15"></td>
+                </tr>
+              </tbody>
+            </table>
+
+            <div style="height:10px;line-height:50px;font-size:7px;"></div>
+
+            <table border="0" cellpadding="0" cellspacing="0" style="border-spacing:0;font-family:sans-serif;color:#333333;" width="100%">
+              <tbody>
+                <tr>
+                  <td style="width:250px;max-width:15px;min-width:15px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" width="15"></td>
+                  <td style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" valign="middle"><a href="https://vk.com/printio" style="outline-style:none;border-width:0;color:#ee6a56;text-decoration:underline;" target="_blank"><img alt="Printio" src="/ru/user_file?resource=images&amp;user_id=1771961&amp;name=/footer/vk.png" style="outline-style:none;border-width:0;" width="24" /></a></td>
+                  <td style="width:10px;max-width:10px;min-width:10px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" width="10"></td>
+                  <td style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" valign="middle"><a href="https://www.facebook.com/printio/" style="outline-style:none;border-width:0;color:#ee6a56;text-decoration:underline;" target="_blank"><img alt="Printio" src="/ru/user_file?resource=images&amp;user_id=1771961&amp;name=/footer/fb.png" style="outline-style:none;border-width:0;" width="24" /></a></td>
+                  <td style="width:10px;max-width:10px;min-width:10px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" width="10"></td>
+                  <td style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" valign="middle"><a href="http://instagram.com/printio" style="outline-style:none;border-width:0;color:#ee6a56;text-decoration:underline;" target="_blank"><img alt="Printio" src="/ru/user_file?resource=images&amp;user_id=1771961&amp;name=/footer/ig.png" style="outline-style:none;border-width:0;" width="24" /></a></td>
+                  <td style="width:250px;max-width:15px;min-width:15px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" width="15"></td>
+                </tr>
+              </tbody>
+            </table>
+
+            <div style="height:9px;line-height:9px;font-size:7px;"></div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div style="height:9px;line-height:9px;font-size:7px;"></div>
+
+      <table border="0" cellpadding="0" cellspacing="0" style="border-spacing:0;font-family:sans-serif;color:#333333;" width="100%">
+        <tbody>
+          <tr>
+            <td style="width:15px;max-width:15px;min-width:15px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" width="15"></td>
+            <td style="padding: 0px; text-align: center;" valign="middle"><font color="#999999" face="Arial, sans-serif" style="font-size:12px;line-height:14px;"><span style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#999999;font-size:12px;line-height:14px;">Вы получили мою рассылку, потому что заказывали на сайте <a href="https://printio.ru/" style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#999999;font-size:12px;line-height:14px;text-decoration:underline;" target="_blank"> <font color="#999999" face="Arial, sans-serif" style="font-size:12px;line-height:14px;text-decoration:underline;"> <span style="font-family:Arial, Tahoma, Geneva, sans-serif;color:#999999;font-size:12px;line-height:14px;text-decoration:underline;">www.printio.ru</span></font></a> </span> </font></td>
+            <td style="width:15px;max-width:15px;min-width:15px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" width="15"></td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div style="height:30px;line-height:30px;font-size:7px;"></div>
 <!-- FOOTER (END) -->
