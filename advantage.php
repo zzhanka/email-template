016b8e2985cab3709a239dd531dde95e0252ea61
Zhanna <danunafik@gmail.com>
2017-10-16 21:42:27 +0300

    Fix

--- advantage.php
+++ advantage.php
@@ -1,12 +1,12 @@
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
   <tr>
-    <td align="center" valign="top" width="25%"><img src="<?=$img5src;?>" border="0" style="display: block;width:108px;max-width:100%;"/><div style="height: 10px; line-height: 10px; font-size: 7px;"></div></td>
+    <td align="center" valign="top" width="25%"><img src="<?=$img5src;?>" border="0" style="display: block;width:109px;max-width:100%;"/><div style="height: 10px; line-height: 10px; font-size: 7px;"></div></td>
     <td style="width: 5px; max-width: 5px; min-width: 5px;" width="5"></td>
-    <td align="center" valign="top" width="25%"><img src="<?=$img6src;?>" border="0" style="display: block;width:108px;max-width:100%;"/><div style="height: 10px; line-height: 10px; font-size: 7px;"></div></td>
+    <td align="center" valign="top" width="25%"><img src="<?=$img6src;?>" border="0" style="display: block;width:109px;max-width:100%;"/><div style="height: 10px; line-height: 10px; font-size: 7px;"></div></td>
     <td style="width: 5px; max-width: 5px; min-width: 5px;" width="5"></td>
-    <td align="center" valign="top" width="25%"><img src="<?=$img7src;?>" border="0" style="display: block;width:108px;max-width:100%;"/><div style="height: 10px; line-height: 10px; font-size: 7px;"></div></td>
+    <td align="center" valign="top" width="25%"><img src="<?=$img7src;?>" border="0" style="display: block;width:109px;max-width:100%;"/><div style="height: 10px; line-height: 10px; font-size: 7px;"></div></td>
     <td style="width: 5px; max-width: 5px; min-width: 5px;" width="5"></td>
-    <td align="center" valign="top" width="25%"><img src="<?=$img8src;?>" border="0" style="display: block;width:108px;max-width:100%;"/><div style="height: 10px; line-height: 10px; font-size: 7px;"></div></td>
+    <td align="center" valign="top" width="25%"><img src="<?=$img8src;?>" border="0" style="display: block;width:109px;max-width:100%;"/><div style="height: 10px; line-height: 10px; font-size: 7px;"></div></td>
   </tr>
   <tr>
     <td align="center" valign="middle"><font color="#000000" face="Arial, sans-serif" style="font-size: 12px; line-height: 14px;"><span style="font-family: Arial, Tahoma, Geneva, sans-serif; color: #000000; font-size: 12px; line-height: 14px;">Оперативную доставку</span></font></td>
